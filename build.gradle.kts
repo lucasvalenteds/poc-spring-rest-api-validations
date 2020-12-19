114e81b750fe96401ed6ba19e47f2474f0b396d4
Lucas <lucasvalenteds@gmail.com>
2020-12-19 13:44:56 -0300

    Configuring persistence

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,15 @@ dependencies {
     testImplementation("org.springframework", "spring-test", properties["version.spring"].toString())
     testImplementation("com.jayway.jsonpath", "json-path", properties["version.jsonpath"].toString())
 
+    implementation("org.springframework.data", "spring-data-jpa", properties["version.spring.data"].toString())
+    implementation("javax.validation", "validation-api", properties["version.javax.validation"].toString())
+    implementation("javax.xml.bind", "jaxb-api", properties["version.jaxb.api"].toString())
+    implementation("org.glassfish.jaxb", "jaxb-runtime", properties["version.jaxb.runtime"].toString())
+    implementation("org.hibernate", "hibernate-core", properties["version.hibernate"].toString())
+    implementation("org.hibernate", "hibernate-entitymanager", properties["version.hibernate"].toString())
+    testImplementation("org.hibernate", "hibernate-testing", properties["version.hibernate"].toString())
+    testImplementation("com.h2database", "h2", properties["version.h2"].toString())
+
     implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
     testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
     implementation("io.projectreactor.netty", "reactor-netty", properties["version.netty"].toString())
--- src/main/java/com/example/spring/Main.java
+++ src/main/java/com/example/spring/Main.java
@@ -3,18 +3,30 @@ package com.example.spring;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 import reactor.netty.http.server.HttpServer;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.time.Duration;
+import java.util.Map;
 
 @EnableWebFlux
+@EnableJpaRepositories
 @Configuration
 @ComponentScan("com.example.spring")
 @PropertySource("classpath:application.properties")
@@ -34,5 +46,35 @@ public class Main {
                 Duration.ofMillis(1000),
                 server -> logger.info("Server running on port {}", server.port())
             );
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(false);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.example.spring");
+        factory.setDataSource(dataSource);
+        factory.setJpaPropertyMap(Map.of("hibernate.format_sql", "true"));
+
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+
+        return txManager;
     }
 }
